4003e567f8f923debb216c9c9a4a168466775e68
Jeff Gonzalez <jeff@hypertheory.com>
2023-02-28 14:54:40 -0500

    api work - employees

---
+++ EmployeesApiSolution/EmployeesApi/Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EmployeesApi.Controllers;
+
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetStatus()
+    {
+        var response = new GetStatusResponse
+        {
+            Message = "Looks good",
+            StatusLevel = StatusLevel.OnFire,
+            HelpContact = new GetStatusContactInfo("David Sands", "555-1555", "dave@aol.com")
+        };
+        return Ok(response);
+    }
+}
