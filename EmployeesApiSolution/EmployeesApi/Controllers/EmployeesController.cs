be2ffe96ff51e670ba73bb50458a7a2513c505e9
Jeff Gonzalez <jeff@hypertheory.com>
2023-03-01 12:23:38 -0500

    can get projected employees

--- EmployeesApiSolution/EmployeesApi/Controllers/EmployeesController.cs
+++ EmployeesApiSolution/EmployeesApi/Controllers/EmployeesController.cs
@@ -1,14 +1,39 @@
 ﻿
 
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeesApi.Controllers;
 
 public class EmployeesController : ControllerBase
 {
+    private readonly EmployeesDataContext _context;
+
+    public EmployeesController(EmployeesDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/employees/summary")]
+    public async Task<ActionResult> GetEmployeeSummary()
+    {
+        return Ok(); // in here give other developers what they need.
+    }
 
     // GET /employees
     [HttpGet("/employees")]
     public async Task<ActionResult> GetAllEmployees()
     {
-        return Ok();
+        var employees = await _context.Employees.OrderBy(e=> e.LastName)
+            .Select(emp => new GetEmployeeSummaryItem
+            {
+                Id = emp.Id.ToString(),
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                Email = emp.Email ?? "No Email Available"
+            })
+            .ToListAsync();
+
+        var response = new GetEmployeeSummary {  Employees = employees };
+        return Ok(response);
     }
 }
---
+++ EmployeesApiSolution/EmployeesApi/Models/EmployeesModels.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeesApi.Models;
+
+
+public record GetEmployeeSummaryItem
+{
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;   
+} 
+
+public record GetEmployeeSummary
+{
+    public List<GetEmployeeSummaryItem> Employees { get; set; } = new();
+}
